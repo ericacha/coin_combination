5a0b51efee0ab3116034cec8c63432148474f8b9
Liz Beacham & Erica Cha <student@epicodus.com>
2015-04-08 11:22:18 -0700

    the coins for fifty cents to return 2 quarters , test pass.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //var coinCombine = function(amount){
 
-function coinCombine(amount) {
+var coinCombine = function(amount) {
     if(amount <= 4) {
         return "You need " + amount + " pennies";
     } else if (amount === 5) {
@@ -9,7 +9,14 @@ function coinCombine(amount) {
         return "You need a dime";
     } else if (amount === 25) {
         return "You need a quarter";
-    }
+    } else if(amount >=25) {
+            var units = amount % 25;
+
+            if(units === 0) {
+                var returned_amount = amount / 25;
+            }
+        }
+        return "You need " + returned_amount + " quarters";
 };
 
 // penny: 1
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,5 @@
  describe('coinCombine', function() {
-     it("tells the user what coin is needed for one cent", function() {
+     it("tells the user what coins are needed for an amount less than five", function() {
          expect(coinCombine(1)).to.equal("You need 1 pennies");
      });
      it("tells the user what coin is needed for five cents", function() {
@@ -10,5 +10,11 @@
      });
      it("tells the user what coin is needed for twenty-five cents", function(){
          expect(coinCombine(25)).to.equal("You need a quarter");
+     });
+    //  it("tells the user what coins are needed for six cents", function() {
+    //      expect(coinCombine(6)).to.equal("You need 1 nickel and 1 penny");
+    //  });
+     it("tells the use what coins are needs for fifty cents", function() {
+         expect(coinCombine(50)).to.equal("You need 2 quarters");
      });
  });
