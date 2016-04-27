bb26406ac3122ac7098222c742be84f999907ae7
cha <chae_osu@yahoo.com>
2016-04-27 00:27:34 -0700

    works

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,23 @@
-//var coinCombine = function(amount){
 
-//INPUT CAN ONLY BE UP TO 25
 var coinCombine = function(amount) {
-    if(amount <= 4) {
-        return "You need " + amount + " pennies";
-    } else if (amount === 5) {
-        return "You need a nickel";
-    } else if (amount === 10) {
-        return "You need a dime";
-    } else if (amount === 25) {
-        return "You need a quarter";
-    } else if (amount < 10) { //amount = 6
-        var pennies = amount % 5; // remainder 1
 
-        if(pennies !== 0) {
-            var cents_remaining  = amount - pennies;
+  var quarters = Math.floor(amount/25);
+  amount = amount - (quarters * 25);
+  var dimes = Math.floor(amount/10);
+  amount = amount - (dimes * 10);
+  var nickels = Math.floor(amount/5);
+  amount = amount - (nickels * 5);
+  var pennies = Math.floor(amount/1);
+  amount = amount - (pennies * 1);
 
-            if(cents_remaining % 5 === 0) {
-                var nickels = cents_remaining / 5;
-            }
-        }
-        return "You need " + nickels + " nickel" + " and " + pennies + " pennies";
-    } else if (amount <= 14) {
-        var pennies = amount % 10;
+  return "You need " + quarters + " quarters, " + dimes + " dimes, " + nickels + " nickels," + " and " + pennies + " pennies";
 
-        if(pennies !== 0) {
-            var cents_remaining = amount - pennies;
 
-            if(cents_remaining % 10 === 0) {
-                var dimes = cents_remaining / 10;
-            }
-        return "You need " + dimes + " dime" + " and " + pennies + " pennies";
-        }
-    } else if(amount === 15) {
-        return "You need a dime and a nickel";
-    } else if(amount >=25) {
-        var units = amount % 25;
-
-        if(units === 0 ) {
-            var quarter = amount / 25;
-            return "You need " + quarter + " quarters";
-        } else {
-            return "Sorry, can't calculate that..."
-        }
-    } else if(amount > 15) {
-        var not_dimes = amount % 10;
-        var pennies = not_dimes % 5;
-        var nickels = (not_dimes - pennies) / 5;
-        var dimes = (amount - not_dimes) / 10;
-
-        return "You need " + dimes + " dimes, " + nickels + " nickels," + " and " + pennies + " pennies";
-    }
 };
+
+
+
+
 
 
 $(document).ready(function() {
@@ -65,14 +31,3 @@ $(document).ready(function() {
     event.preventDefault();
     });
 });
-
-
-// else if (units !== 0) {
-//     var not_quarters = amount % 25;
-//     var pennies = not_quarters % 10;
-//     var nickels = pennies % 5;
-//     var nickel_cents =
-//     var quarter = (amount - not_quarters) / 25;
-//     var quarter_cents = amount - not_quarters;
-//     var dimes = (amount - quarter_cents - pennies) / 10;
-// }
